077b0e323abba494957bda5dbe23478e3bc13582
sunset12321215 <sunsetno08@gmail.com>
2019-01-25 09:15:57 +0700

    Initial Commit

---
+++ Delegate Pattern/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Delegate Pattern
+//
+//  Created by mac on 1/25/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
